e7bdfbcdb835bccaae6399d3c96d2d0bb9a4c384
Mohammad Suliman joya <sjoya66@gmail.com>
2022-11-25 21:44:08 +0430

    Fix linter errors

--- JS/formValidation.js
+++ JS/formValidation.js
@@ -2,7 +2,7 @@ const form = document.getElementById('formContainer');
 const email = document.getElementById('email');
 const error = document.getElementById('error-msg');
 
-form.addEventListener('submit',  (e) => {
+form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     error.textContent = '* Please write your email in lower case.';
     e.preventDefault();
